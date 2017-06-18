e123f31a2a863491bb6353336038e7d324475bc9
sc0tfree <henry@sc0tfree.com>
2017-06-18 19:01:34 -0400

    Add setuptools for install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='netbyte',
+    version='0.4',
+    url='http://www.sc0tfree.com',
+    license='MIT License',
+    author='sc0tfree',
+    author_email='henry@sc0tfree.com',
+    description='Netbyte is a Netcat-style tool that facilitates probing proprietary TCP and UDP services. It is lightweight, fully interactive and provides formatted output in both hexadecimal and ASCII.',
+    keywords='utils cli netcat hexadecimal',
+    packages=['netbyte'],
+    install_requires=[
+        'colorama',
+    ],
+    entry_points = {
+        "console_scripts" : ['netbyte = netbyte.netbyte:main']
+    },
+)
