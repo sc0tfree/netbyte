863b215235601bd97b524b87afc2e6d33ab89167
sc0tfree <henry@sc0tfree.com>
2017-09-11 13:53:02 -0400

    Move output functions from main to output module

--- netbyte/netbyte.py
+++ netbyte/netbyte.py
@@ -18,31 +18,6 @@ from threading import Thread
 from Queue import Queue, Empty
 
 
-def print_ascii(string):
-    '''
-    Print string with ASCII color configuration
-    '''
-    if string.isspace():
-        # Add a space to show colors on a non-ASCII line
-        string = ' ' + string
-    print(Fore.MAGENTA + Style.BRIGHT + string + Style.RESET_ALL)
-
-
-def print_hex(string):
-    '''
-    Print string with hex color configuration
-    '''
-    print(Fore.BLUE + Style.BRIGHT + string + Style.RESET_ALL)
-
-
-def print_error(string):
-    '''
-    Print string with error color configuration and exit with code 1
-    '''
-    print(Fore.RED + Style.BRIGHT + string + Style.RESET_ALL)
-    exit(1)
-
-
 class ReadAsync(object):
     '''
     ReadAsync starts a queue thread to accept stdin
--- netbyte/util/output.py
+++ netbyte/util/output.py
@@ -11,3 +11,27 @@
 # netbyte.util.output module
 #
 
+
+def print_ascii(string):
+    '''
+    Print string with ASCII color configuration
+    '''
+    if string.isspace():
+        # Add a space to show colors on a non-ASCII line
+        string = ' ' + string
+    print(Fore.MAGENTA + Style.BRIGHT + string + Style.RESET_ALL)
+
+
+def print_hex(string):
+    '''
+    Print string with hex color configuration
+    '''
+    print(Fore.BLUE + Style.BRIGHT + string + Style.RESET_ALL)
+
+
+def print_error(string):
+    '''
+    Print string with error color configuration and exit with code 1
+    '''
+    print(Fore.RED + Style.BRIGHT + string + Style.RESET_ALL)
+    exit(1)
