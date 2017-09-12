d211654b883a68eccb591ab7902fb90b9592e760
sc0tfree <henry@sc0tfree.com>
2017-09-11 23:31:28 -0400

    Update setup.py information

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     license='MIT License',
     author='sc0tfree',
     author_email='henry@sc0tfree.com',
-    description='Netbyte is a Netcat-style tool that facilitates probing proprietary TCP and UDP services. It is lightweight, fully interactive and provides formatted output in both hexadecimal and ASCII.',
-    keywords='utils cli netcat hexadecimal',
+    description='Netbyte is a Netcat-style tool for manual probing, fuzzing and exploitation of TCP and UDP services. It is lightweight, fully interactive and provides formatted output in both hexadecimal and ASCII.',
+    keywords='fuzzing netcat utils cli hexadecimal',
     packages=['netbyte', 'netbyte.util'],
     install_requires=[
         'colorama',
