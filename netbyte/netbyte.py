5d9bc97113205340e5ac03328b2bd0a2bc3a3f67
sc0tfree <henry@sc0tfree.com>
2017-09-11 21:28:58 -0400

    Add and implement util.network module

--- netbyte/netbyte.py
+++ netbyte/netbyte.py
@@ -8,15 +8,8 @@
 #                       Twitter: @sc0tfree
 #                       Email: henry@sc0tfree.com
 
-import socket
-import errno
-import sys
-import time
-from Queue import Empty
 import util.cli as cli
-import util.output as out
-import util.text as text
-from util.readasync import ReadAsync
+import util.network as net
 
 
 def main():
@@ -26,44 +19,4 @@ def main():
 
     args = cli.parse_arguments()
 
-    if args.udp:
-        connection = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    else:
-        connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    connection.settimeout(2)
-
-    address = (args.hostname, int(args.port))
-
-    try:
-        connection.connect(address)
-
-    except socket.error:
-        out.print_error("Could not establish connection to " + address[0] + ":" + str(address[1]))
-
-    # print(Fore.GREEN + Style.BRIGHT + "Connection established" + Style.RESET_ALL)
-
-    try:
-        connection.setblocking(0)
-        stdin = ReadAsync(sys.stdin.readline)
-
-        while True:
-            try:
-                data = connection.recv(4096)
-                if not data:
-                    raise socket.error
-                out.print_ascii(data)
-                out.print_hex(text.to_hex(data))
-            except socket.error, e:
-                if e.errno != errno.EWOULDBLOCK:
-                    raise
-            try:
-                connection.send(stdin.dequeue())
-            except Empty:
-                time.sleep(0.1)
-
-    except KeyboardInterrupt:
-        connection.close()
-        out.print_error("\nExiting...")
-    except socket.error:
-        out.print_error("Connection closed")
+    net.connect(args)
