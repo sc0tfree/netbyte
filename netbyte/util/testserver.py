cbb721d5e61ec0c915f3f718879cf95a88dd03c2
sc0tfree <henry@sc0tfree.com>
2017-09-16 18:19:34 -0400

    Rewrite test server

--- netbyte/util/testserver.py
+++ netbyte/util/testserver.py
@@ -11,9 +11,132 @@
 # netbyte.util.testserver module
 #
 
+import re
+import errno
 import socket
 import text
 import output as out
+from .. import __version__ as VERSION
+
+
+class DisconnectedError(Exception):
+    '''
+    This indicates that the connection has been closed before completion of
+    the tests.
+    '''
+    pass
+
+
+end_pattern = re.compile('^\s*end\s*', re.IGNORECASE)
+
+
+def handle_connection(connection, addr):
+    '''
+    A client has connected. Run through the tests and then disconnect.
+    '''
+    connection.send("Netbyte Test Server " + VERSION + "\n")
+
+    def run_test(test_name, prompt, data_handler, initial_message=""):
+        '''
+        Runs a test repeatedly until the client ends it
+
+        test_name: This is displayed as the test name
+        prompt: This prompt string is sent for each test loop
+        data_handler: A function to call on each line received from the client
+        initial_message: This is used to pass on a remaining message from a
+            previous call to run_test
+
+        returns: Any remaining message recieved after the test is ended, or ""
+        '''
+        out.print_flat(test_name + ":")
+        connection.send(test_name + ":\nType 'end' to exit\n")
+        messages = []
+        remaining_message = ""
+
+        done = False
+        while not done:
+            if prompt:
+                connection.send(prompt)
+
+            if initial_message:
+                message = initial_message
+                initial_message = ""
+            else:
+                message = connection.recv(4096)
+
+            if message:
+                messages.append(message)
+            else:
+                raise DisconnectedError()
+
+            if "\n" in message:
+                # Consume the message line by line
+                start_i = 0
+                end_i = message.find("\n")
+                while end_i != -1 and not done:
+                    line = message[start_i: end_i]
+                    if end_pattern.match(line):
+                        done = True
+                    else:
+                        data_handler(line)
+
+                    start_i = end_i + 1
+                    end_i = message.find("\n", start_i)
+
+                if start_i < len(message) - 1:
+                    if done:
+                        remaining_message = message[start_i:]
+                    else:
+                        messages = [message[start_i:]]
+
+        return remaining_message
+
+    def echo_handler(line):
+        '''
+        Receives a string, sends it back
+        '''
+        out.print_flat("Received: " + line.rstrip())
+        connection.send(line + "\n")
+
+    def hex_handler(line):
+        '''
+        Receives a number, sends back that number of random bytes
+        '''
+        try:
+            hex_length = int(line)
+            if hex_length <= 0:
+                raise ValueError
+        except ValueError:
+            connection.send("You must enter a number larger than 0. Defaulting to 10.\n")
+            hex_length = 10
+        hex_string = text.generate_random_hex(hex_length)
+        out.print_flat("Sending: " + hex_string)
+        connection.send(hex_string)
+
+    def byte_count_handler(line):
+        '''
+        Recieves a string, sends back the number of bytes in the string
+        '''
+        size = len(line)
+        response = "Received %d bytes." % size
+        out.print_flat(response)
+        connection.send(response + "\n")
+
+    remaining_msg = run_test("Echo Test", "", echo_handler)
+    remaining_msg = run_test("Hex Test", "Number of bytes to send:",
+                             hex_handler, remaining_msg)
+    remaining_msg = run_test("Byte Count Test", "Bytes to count:",
+                             byte_count_handler, remaining_msg)
+
+    if remaining_msg:
+        info = "Received unexpected data after all tests ended:" + remaining_msg
+        out.print_info(info)
+        connection.send(info)
+
+    connection.send("Closing connection to test server\n")
+    connection.close()
+    out.print_info("Closed connection to " + str(addr[0]) + ':' + str(addr[1]))
+
 
 def launch_testserver(port):
     '''
@@ -21,59 +144,43 @@ def launch_testserver(port):
     '''
     host = '127.0.0.1'
 
-    out.print_info('Starting test server on 127.0.0.1:' + str(port))
+    out.print_info("Starting test server on %s:%d" % (host, port))
 
     s = socket.socket()
+
+    # allow quick restarts of the server
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     try:
         s.bind((host, port))
     except socket.error as e:
-        if e.errno == 13:
+        if e.errno == errno.EACCES:
             out.print_error("Permission denied: try again as superuser")
-        elif e.errno == 48:
+        elif e.errno == errno.EADDRINUSE:
             out.print_error("Port is already in use")
     except OverflowError:
-            out.print_error("Port must be between 1 and 65535")
+        out.print_error("Port must be between 1 and 65535")
 
     s.listen(5)
 
     try:
-
         while True:
-
-            c, addr = s.accept()
-
-            print 'Connection established from', addr[0], ':', addr[1]
-
-            c.send('Hello from Test Server\n')
-
-            # Echo Test
-            c.send('Echo Test - enter string:')
-            data = c.recv(1024)
-            print 'Echo Test - received: ', data
-            c.send('Echo Test - received: ' + data + '\n')
-
-            # Hex Test
-            c.send('Hex Test - enter length:')
-            data = c.recv(1024)
-
             try:
-                hex_length = int(data)
-            except ValueError:
-                c.send('You must enter a number. Defaulting to 10.\n')
-                hex_length = 10
+                c, addr = s.accept()
+                out.print_info("Connection established from %s:%d" % addr)
+                handle_connection(c, addr)
 
-            hex_string = text.generate_random_hex(hex_length)
-            c.send('Sending hex string...\n\n')
-            print 'Hex Test - sending: ', hex_string
-            c.send(hex_string)
+            except socket.error as e:
+                if e.errno == errno.EPIPE:
+                    out.print_info("Connection closed by user.")
+                else:
+                    out.print_info("Socket error: %s" % errno.errorcode[e.errno])
 
-            c.close()
-            print 'Closed connection to ', addr[0], ':', addr[1]
+            except DisconnectedError:
+                out.print_info("Connection closed.")
 
+    except ValueError:
+        pass
     except KeyboardInterrupt:
-        try:
-            c.close()
-        except NameError:
-            pass
-        out.print_error('\nExiting...')
+        s.close()
+        out.print_error("\nExiting...")
