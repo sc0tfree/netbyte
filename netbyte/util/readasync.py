1cb81d26469f229227c907d636aab30e23f90a76
sc0tfree <henry@sc0tfree.com>
2017-09-19 14:10:39 -0400

    Add documentation and fix formatting on eval function

--- netbyte/util/readasync.py
+++ netbyte/util/readasync.py
@@ -17,12 +17,17 @@ from Queue import Queue
 import output as out
 
 
-def eval_expressions(string):
+def eval_expression(string):
+    '''
+    Searches for an instance of !! in the string, and if found, replaces the
+    remaining string with the result of eval()ing it as Python code. This is
+    used in fuzzing, for example to produce repeated instances of a character.
+    '''
     p = re.compile('^\s*!!([\s\S]*)')
     m = p.match(string)
     if m:
         try:
-            evaluated = eval(m.group(1), {'__builtins__':None}, {})
+            evaluated = eval(m.group(1), {'__builtins__': None}, {})
         except (SyntaxError, ValueError, TypeError, NameError):
             out.print_info('Incorrectly formatted statement. Please try again.')
             return ''
@@ -51,7 +56,7 @@ class ReadAsync(object):
     def enqueue(self):
         while True:
             buffer = self.read(*self.args)
-            buffer_e = eval_expressions(buffer)
+            buffer_e = eval_expression(buffer)
             self.queue.put(buffer_e)
 
     def dequeue(self):
