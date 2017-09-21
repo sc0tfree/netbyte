efa0c10414c742d61271825a85af47ae5d64dd65
sc0tfree <henry@sc0tfree.com>
2017-09-20 21:26:01 -0400

    Move to_hex inside of network module

--- netbyte/network.py
+++ netbyte/network.py
@@ -18,8 +18,46 @@ import time
 from Queue import Empty
 
 import output as out
-import text
 from readasync import ReadAsync
+
+
+def to_hex(string):
+    '''
+    Converts byte string to formatted hex and ASCII reference values
+
+    Returns:
+        str: Formatted hex & ASCII reference
+    '''
+
+    symbols = "~`!@#$%^&*()_-+={}[]:>;',</?*-+"
+
+    results = []
+
+    new_line = True
+
+    for byte in string:
+        # Convert to upper-case hexadecimal with two places
+        hex_value = '%02X' % ord(byte)
+
+        # Add reference ASCII for readability
+        if byte.isalpha() or byte.isdigit() or byte in symbols:
+            hex_value = hex_value + '(' + byte + ')'
+
+        # Add a space in between hex values (not to a new line)
+        if not new_line:
+            hex_value = ' ' + hex_value
+
+        # Add a newline for readability (corresponding to ASCII)
+        if '0A' in hex_value and not string.isspace():
+            hex_value = hex_value + '(\\n)\n'
+            # Next line will be a newline
+            new_line = True
+        else:
+            new_line = False
+
+        results.append(hex_value)
+
+    return ''.join(results)
 
 
 def connect(args):
